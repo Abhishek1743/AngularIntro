49cac88644bf9690ed113e705548a53ecbe8f269
Abhishek1743 <abhisheksagar1113@gmail.com>
2025-03-24 10:40:55 +0530

    Added Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']  // Optional if you want styles
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
-
-  ngOnInit(): void {
-    this.title = "Hello From BridgeLabz"
-  }
+  logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';
 }
